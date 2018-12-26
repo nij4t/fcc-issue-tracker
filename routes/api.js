f563a885933aae58306cc14e6b31f98543b5c64e
Nijat Mahmudov <n.e.mahmudov@gmail.com>
2018-12-26 16:52:55 +0400

    Implement POST /api/issues/{projectname} endpoint

--- routes/api.js
+++ routes/api.js
@@ -1,41 +1,58 @@
 /*
-*
-*
-*       Complete the API routing below
-*
-*
-*/
+ *
+ *
+ *       Complete the API routing below
+ *
+ *
+ */
 
-'use strict';
+"use strict";
 
-var expect = require('chai').expect;
-var MongoClient = require('mongodb');
-var ObjectId = require('mongodb').ObjectID;
+var expect = require("chai").expect;
+var MongoClient = require("mongodb").MongoClient;
+var ObjectId = require("mongodb").ObjectID;
 
-const CONNECTION_STRING = process.env.DB; //MongoClient.connect(CONNECTION_STRING, function(err, db) {});
+const CONNECTION_STRING = process.env.MONGOLAB_URI; //MongoClient.connect(CONNECTION_STRING, function(err, db) {});
 
-module.exports = function (app) {
+module.exports = function(app) {
+  app
+    .route("/api/issues/:project")
 
-  app.route('/api/issues/:project')
-  
-    .get(function (req, res){
+    .get(function(req, res) {
       var project = req.params.project;
-      
     })
-    
-    .post(function (req, res){
+
+    .post(function(req, res) {
       var project = req.params.project;
-      
+      const issue = {
+        issue_title: req.body.issue_title,
+        issue_text: req.body.issue_text,
+        created_by: req.body.created_by,
+        created_on: new Date(),
+        updated_on: new Date(),
+        open: true,
+        assigned_to: req.body.assigned_to || "",
+        status_text: req.body.status_text || ""
+      };
+
+      if (!issue.issue_title || !issue.issue_text || !issue.created_by)
+        res.json({ error: "missing inputs" });
+
+      MongoClient.connect(CONNECTION_STRING).then(db => {
+        db.collection(project)
+          .insertOne(issue)
+          .then(doc => {
+            issue._id = doc.insertedId;
+            res.json(issue);
+          });
+      });
     })
-    
-    .put(function (req, res){
+
+    .put(function(req, res) {
       var project = req.params.project;
-      
     })
-    
-    .delete(function (req, res){
+
+    .delete(function(req, res) {
       var project = req.params.project;
-      
     });
-    
 };
